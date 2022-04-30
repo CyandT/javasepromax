edcf20161148175d650a543760d36546949bde7a
cyandT <1821946709@qq.com>
2022-04-30 15:33:12 +0800

    abstract

---
+++ day02-oop-demo/src/d7_abstract_demo/Test.java
@@ -0,0 +1,11 @@
+package d7_abstract_demo;
+
+public class Test {
+    public static void main(String[] args) {
+        WhiteGold n = new WhiteGold();
+        n.setName("刘**");
+        n.setYuE(60000);
+        n.pay(50000);
+
+    }
+}
