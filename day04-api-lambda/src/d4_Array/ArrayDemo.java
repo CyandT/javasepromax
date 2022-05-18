c48e6a8e3b0e0971cd557b0d135631529591c692
cyandT <1821946709@qq.com>
2022-05-19 00:02:29 +0800

    Array

--- day04-api-lambda/src/d4_Array/ArrayDemo.java
+++ day04-api-lambda/src/d4_Array/ArrayDemo.java
@@ -1,13 +1,26 @@
 package d4_Array;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ArrayDemo {
     public static void main(String[] args) {
-        int [] arr = {11, 55, 23, 89};
+        int []arr = {11,55,23,69,84,45};
+        Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
-
-        int index = Arrays.binarySearch(arr,55);
-        System.out.println(index);
+        int index = Arrays.binarySearch(arr,69);
+        System.out.println("69在第" + index + "位");
+/**
+ * 倒序
+ * 只支持引用类型
+ */
+        Integer []arr1 = {11,55,23,69,84,45};
+        Arrays.sort(arr1, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        System.out.println(Arrays.toString(arr1));
     }
 }
